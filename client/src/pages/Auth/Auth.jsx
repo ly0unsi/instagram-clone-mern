f3a59cd69de801146a58eb9eb22f9e93ba762fa8
ly0unsi <mcabdo25@gmail.com>
2022-07-13 15:35:55 +0100

    commit

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,112 +1,146 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
 
 const Auth = () => {
-  const [issignup, setIsSignup] = useState(false)
+  const [isSignUp, setIsSignUp] = useState(false)
+  const [data, setdata] = useState({firstname:"",lastname:"",password:"",confirmpass:"",username:""})
+  const [confirmPass, setconfirmPass] = useState(true)
+  const [buttDis,setButtDis]=useState(false)
+  const handleChange=(e)=>{
+   
+    setdata({...data,[e.target.name]:e.target.value})
+   
+  }
+  const resetForm =()=>{
+    setdata({firstname:"",lastname:"",password:"",confirmpass:"",username:""})
+  }
+  useEffect(()=>{
+    if (data.password!==data.confirmpass && isSignUp) {
+      setconfirmPass(false)
+      setButtDis(true)
+     }else{
+       setconfirmPass(true)
+       setButtDis(false)
+     }
+  },[data.confirmpass,data.password,isSignUp])
   return (
     <div className="Auth">
-      <div className="a-left">
-        <img src={Logo} alt="" />
-        <div className="Webname">
-          <h1>ZKC Media</h1>
-          <h6>Explore the ideas throughout the world</h6>
-        </div>
+    {/* left side */}
+
+    <div className="a-left">
+      <img src={Logo} alt="" />
+
+      <div className="Webname">
+        <h1>ZKC Media</h1>
+        <h6>Explore the ideas throughout the world</h6>
       </div>
-      {issignup?<SignUp issignup={issignup} setIsSignup={setIsSignup}/>:<LogIn/>}
     </div>
-  );
-};
-function LogIn({issignup, setIsSignup}) {
-    return (
-      <div className="a-right">
-        <form className="infoForm authForm">
-          <h3>Log In</h3>
-  
-          <div>
-            <input
-              type="text"
-              placeholder="Username"
-              className="infoInput"
-              name="username"
-            />
-          </div>
-  
-          <div>
-            <input
-              type="password"
-              className="infoInput"
-              placeholder="Password"
-              name="password"
-            />
-          </div>
-  
-          <div>
-              <span style={{ fontSize: "12px" }}>
-                Don't have an account? 
-              </span>
-              <span onClick={()=>setIsSignup((prev)=>!prev)}>
-                Sign up
-              </span>
-            <button className="button infoButton">Login</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-function SignUp({issignup, setIsSignup}) {
-  return (
+
+    {/* right form side */}
+
     <div className="a-right">
-      <form className="infoForm authForm">
-        <h3>Sign up</h3>
+      <form className="infoForm authForm" >
+        <h3>{isSignUp ? "Register" : "Login"}</h3>
+        {isSignUp && (
+          <div>
+            <input
+              required
+              type="text"
+              placeholder="First Name"
+              className="infoInput"
+              name="firstname"
+              onChange={handleChange}
+              value={data.firstname}
+            />
+            <input
+              required
+              type="text"
+              placeholder="Last Name"
+              className="infoInput"
+              name="lastname"
+              onChange={handleChange}
+              value={data.lastname}
+            />
+          </div>
+        )}
 
         <div>
           <input
+            required
             type="text"
-            placeholder="First Name"
-            className="infoInput"
-            name="firstname"
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="infoInput"
-            name="lastname"
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
+            placeholder="Username"
             className="infoInput"
             name="username"
-            placeholder="Usernames"
+            onChange={handleChange}  
+            value={data.username}         
           />
         </div>
-
         <div>
           <input
-            type="text"
+            required
+            type="password"
             className="infoInput"
-            name="password"
             placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={data.password}
           />
-          <input
-            type="text"
-            className="infoInput"
-            name="confirmpass"
-            placeholder="Confirm Password"
-          />
+          {isSignUp && (
+            <input
+              required
+              type="password"
+              className="infoInput"
+              name="confirmpass"
+              placeholder="Confirm Password"
+              onChange={handleChange}
+              value={data.confirmpass}
+            />
+          )}
         </div>
 
+        <span
+          style={{
+            color: "red",
+            fontSize: "12px",
+            alignSelf: "flex-end",
+            marginRight: "5px",
+            display:confirmPass ? "none":"block",
+          }}
+        >
+          *Confirm password is not same
+        </span>
         <div>
-            <span style={{fontSize: '12px'}}>Already have an account.</span>
-            <span onClick={()=>setIsSignup((prev)=>!prev)}> Login!</span>
+          <span
+            style={{
+              fontSize: "12px",
+              cursor: "pointer",
+              textDecoration: "underline",
+            }}
+            onClick={() => {
+              setIsSignUp((prev) => !prev);
+              resetForm()
+            }}
+          >
+            {isSignUp
+              ? "Already have an account Login"
+              : "Don't have an account Sign up"}
+          </span>
+          <button
+            className="button infoButton"
+            type="Submit"
+            style={{
+              background:buttDis&&"gray",
+              cursor:buttDis && "none"
+            }}
+            disabled={buttDis}
+          >
+            {isSignUp ? "SignUp" : "Login"}
+          </button>
         </div>
-        <button className="button infoButton" type="submit">Signup</button>
       </form>
     </div>
+  </div>
   );
-}
-
+};
 export default Auth;
