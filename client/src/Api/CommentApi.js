9b31bbe0455058d21cdb49a7c3a1fc9366cdc8bc
ly0unsi <mcabdo25@gmail.com>
2022-08-01 16:37:44 +0100

    commit

--- client/src/Api/CommentApi.js
+++ client/src/Api/CommentApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const Api =axios.create({baseURL:"https://rs-mern.herokuapp.com/"})
+const Api =axios.create({baseURL:"http://localhost:5000"})
 export const addComment =(formData)=> Api.post('comment/add',formData)
 export const getPostComments=(postId)=>Api.get(`comment/postComments/${postId}`)
 export const deleteComment=(userId,commentId)=>Api.delete(`comment/delete/${commentId}`,{data:{currentUserId:userId}})
--- client/src/components/PostSide/PostSide.jsx
+++ client/src/components/PostSide/PostSide.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Posts from '../Posts/Posts'
 import PostShare from '../PostShare/PostShare'
 import './PostSide.css'
-const PostSide = ({profileUser}) => {
+const PostSide = () => {
   return (
     <div>
         <PostShare/>
-        <Posts profileUser={profileUser}/>
+        <Posts/>
     </div>
   )
 }
