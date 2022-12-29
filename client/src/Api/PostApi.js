14ad1992884df502a96f3bc288abc6d378e36a1f
ly0unsi <mcabdo25@gmail.com>
2022-12-29 21:23:03 +0100

    commit

--- client/src/Api/PostApi.js
+++ client/src/Api/PostApi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const API = axios.create({ baseURL: "http://localhost:5000" })
 export const getTimelinePosts = (id) => API.get(`post/timelineposts/${id}`);
 export const likePost = (id, data) => API.put(`post/like/${id}`, { userId: data.userId, shared: data.shared });
-export const deletePost = (postId, userId) => API.delete(`post/delete/${postId}`, { data: { userId: userId } })
+export const deletePost = (postId, data) => API.delete(`post/delete/${postId}`, { data: { userId: data.userId, shared: data.shared } })
 export const updatePost = (postId, updatedPost) => API.put(`post/update/${postId}`, updatedPost)
 export const getNots = (userId) => API.get(`notifs/get/${userId}`)
 export const readNot = (id) => API.put(`notifs/readNot/${id}`)
